9086daab7d41e8da8ff4caf820c4f907092bd63b
IceSentry <c.giguere42@gmail.com>
2017-11-11 01:31:56 -0500

    Trying to generate all the faces correctly

--- Assets/Scripts/PlanetGenerator.cs
+++ Assets/Scripts/PlanetGenerator.cs
@@ -34,7 +34,43 @@ public class PlanetGenerator : MonoBehaviour
     {
         Debug.Log("Generate started");
 
-        new GameObject().AddComponent<Grid>().Initialize(gameObject, gridSize, radius);
+        var faces = new List<Face>();
+
+        var faceForward = new GameObject().AddComponent<Face>();
+        faceForward.Initialize(gameObject, gridSize, radius, "forward");
+        faces.Add(faceForward);
+
+        //var faceBack = new GameObject().AddComponent<Face>();
+        //faceBack.Initialize(gameObject, gridSize, radius, "back");
+        //faces.Add(faceBack);
+
+        var faceUp = new GameObject().AddComponent<Face>();
+        faceUp.Initialize(gameObject, gridSize, radius, "up");
+        faces.Add(faceUp);
+
+        //new GameObject().AddComponent<Face>().Initialize(gameObject, gridSize, radius, "down");
+        //new GameObject().AddComponent<Face>().Initialize(gameObject, gridSize, radius, "left");
+        //new GameObject().AddComponent<Face>().Initialize(gameObject, gridSize, radius, "right");
+
+        foreach (var face in faces)
+        {
+            var mesh = face.GetComponent<MeshFilter>().mesh;
+            var vertices = mesh.vertices;
+            var normals = new Vector3[vertices.Length];
+            var colors = new Color32[vertices.Length];
+            
+            for (int i = 0; i < vertices.Length; i++)
+            {
+                var v = vertices[i];
+                var noiseValue = noise.GetValueFractal(v.x, v.y, v.z);
+                vertices[i] = v.normalized * (radius + noiseValue * noisemod);
+                normals[i] = vertices[i];
+                colors[i] = Color.Lerp(Color.blue, Color.green, noiseValue);
+            }
+
+            mesh.vertices = vertices;
+            mesh.colors32 = colors;
+        }
 
         Debug.Log("Generate finished");
     }
